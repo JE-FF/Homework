ad79fe40d741aa52225d138c25668afaaf868916
Jeff <jeffrey.edson@pm.me>
2021-11-11 20:14:26 -0600

    initial commit

---
+++ Section02/pizzaPlace.js
@@ -0,0 +1,17 @@
+let pizzaPlace = "Jeff's Pizza";
+let numberOfToppings = 11;
+
+console.log(pizzaPlace + " " + typeof(pizzaPlace));
+console.log(numberOfToppings + " " + typeof(numberOfToppings));
+
+console.log(pizzaPlace  + ` serves ` + numberOfToppings + ` kinds of toppings.`);
+
+if (numberOfToppings >= 10) {
+    console.log("A whole lot of pizza.");
+} else {
+    console.log("Quality, not quantity.");
+}
+
+for (i = 2; i < numberOfToppings; i += 2) {
+    console.log(i);
+}
